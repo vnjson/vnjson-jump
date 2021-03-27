c3104a08700864f1d1c7b805419812bb41dab1df
Kasin Sergey <bakakasin@gmail.com>
2021-03-27 17:37:18 +0700

    vnjson.js@1.5.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-function jump(pathname){
+function jumpVnjson(){
+
+this.on('jump', pathname=>{
+
   let path = pathname.split('.');
-	this.index = 0;
-	//label
-	if(path.length===1){
-		this.currentLabelName = path[0];
-		this.exec();
-	}
-	//scene.label
-	if(path.length===2){
-			this.currentSceneName = path[0];
-			this.currentLabelName = path[1];
-			let { scenes, order, loader } = this.sceneLoadConfig;
-			if(order==='once'){
-				var arr = scenes.filter(item=>{ return item.name===path[0];})
-				loader(arr[0], _=>{
-						this.exec();
-				});
-			}else{
-						this.exec();
-			};
-	};
-};
+
+  this.current.index = 0;
+  //label
+  if(!/\./i.test(pathname)){
+					
+	  this.current.labelName = path[0];
+	  this.emit('init', false)
+  }
+  //scene.label
+  if(/\./i.test(pathname)){
+		  this.current.sceneName = path[0];
+		  this.current.labelName = path[1];
+		  this.emit('init', true)
+
+  };  })
+
+}
